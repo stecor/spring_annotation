5e86860bb80e317514d0f7f587ee9271df8bfd54
Stefano Corra <stefano.corra@hotmail.com>
2017-06-27 15:50:53 -0400

    second commit

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,3 +1,6 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import com.pluralsight.service.CustomerService;
 import com.pluralsight.service.CustomerServiceImpl;
 
@@ -5,9 +8,11 @@ public class Application {
 
 	public static void main(String[] args) {
 		
-		CustomerService service = new CustomerServiceImpl();
+		ApplicationContext appContext = new ClassPathXmlApplicationContext("applicationContext.xml");
 		
-	 System.out.println(service.findAll().get(0).getFirstname());
+		CustomerService service = appContext.getBean("customerService", CustomerService.class);
+		
+		System.out.println(service.findAll().get(0).getFirstname());
 		
 	}
 
